4cb6ffacdae01e77dc490a091a9758e5bd8b5037
moricandia <sg4122@columbia.edu>
2025-02-18 11:54:51 -0500

    Added the chunk of code from Nick that extracts experiment info from file names for harmonization

--- scripts/Data-Harmonization.R
+++ scripts/Data-Harmonization.R
@@ -1,3 +1,22 @@
 install.packages("librarian")
 librarian::shelf(ltertools, lterdatasampler, psych, supportR, tidyverse)
 
+# Example code from Nick for how to extract information from file names directly for data harmonization
+# Break useful information out of file name ("source" column)
+combo_v7 <- combo_v6 %>% 
+  # Separate by underscore
+  tidyr::separate_wider_delim(cols = source, delim = "_",
+                              names = c("organization", "site", 
+                                        "experiment.name", "sampling.years", 
+                                        "excluded.group", "measured.group"),
+                              cols_remove = F) %>%
+  # Remove file name from final bit of file
+  dplyr::mutate(measured.group = gsub(pattern = "\\.csv", replacement = "",
+                                      x = measured.group)) %>% 
+  # Relocate source back to first position
+  dplyr::relocate(source, .before = dplyr::everything())
+
+# Check structure
+dplyr::glimpse(combo_v7)
+
+
