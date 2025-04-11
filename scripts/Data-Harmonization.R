b61526e261980a7e1b004cce62dda0cb0ecf7787
Ian-Yiyang <yiyangxu@bu.edu>
2025-04-11 10:35:56 -0400

    initial commit April 11

--- scripts/Data-Harmonization.R
+++ scripts/Data-Harmonization.R
@@ -1,3 +1,12 @@
 install.packages("librarian")
-librarian::shelf(ltertools, lterdatasampler, psych, supportR, tidyverse)
+librarian::shelf(ltertools, lterdatasampler, psych, supportR, tidyverse, usethis, here)
 
+# Define URL as an object
+dt_url <- "https://raw.githubusercontent.com/lter/ssecr-c-cycling/refs/heads/main/data/Konza%20Prairie%20Biomass.csv"
+
+# Read it into R
+Konza <- read_csv(file = dt_url)
+
+Konza_clean <- Konza %>%
+  rename(Year = RECYEAR, Month = RECMONTH) %>%
+  mutate(Month = month.name[Month])
