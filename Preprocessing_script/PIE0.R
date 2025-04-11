b241bd421516086ce8b8b21bd1f8f2c3e628b151
Ian-Yiyang <yiyangxu@bu.edu>
2025-04-11 17:34:29 -0400

    initiating the pre-processing created the Preprocessing_script folder created PIE0.R which is the script that could be used for other sites too created the folder Ready_data created initial ready data PIE1.csv

---
+++ Preprocessing_script/PIE0.R
@@ -0,0 +1,37 @@
+# Load necessary package
+library(dplyr)
+library(readr)
+
+# Your file ID: find the share link for your data file,
+# The file ID is the part between /d/ and /view?, 
+# don't forget to make the file open to anyone with the link
+file_id <- "1WOk0ED2-XU2p0fTrx2Zx0c8G8NDGLJWg"
+
+# Construct the download URL
+url <- sprintf("https://drive.google.com/uc?export=download&id=%s", file_id)
+
+# Read the CSV file
+df <- read.csv(url)
+
+
+
+# General structure and summary, 
+#don't have to run this but good to know the column names
+
+str(df)              # Structure of the dataset
+summary(df)          # Summary statistics for each column
+head(df)             # First few rows
+
+# This is a customized function 
+# to summarize all the numbers with the same value in
+# the mentioned columns, for example, here we are summarizing
+# any data with the same Year, Species, Creek... etc
+
+sum_df <- summarize_by_columns(df, c("Year", "Species","Creek","Branch","Transect"))
+
+# saving the processed data to a new spreadsheet and put it into the ready folder!
+# Sip your coffee and carry on to the next dataset!
+
+write.csv(sum_df, "Ready_data/PIE1.csv", row.names = FALSE)
+
+
