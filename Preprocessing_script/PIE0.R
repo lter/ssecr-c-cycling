0ce8de88c3b24e53a29faeb04fa7be0aefd50c78
Ian-Yiyang <yiyangxu@bu.edu>
2025-05-02 14:50:30 -0400

    processed NWT_AbovegroundBiomass_2006-2019_gm2

--- Preprocessing_script/PIE0.R
+++ Preprocessing_script/PIE0.R
@@ -5,8 +5,8 @@ library(readr)
 # Your file ID: find the share link for your data file,
 # The file ID is the part between /d/ and /view?, 
 # don't forget to make the file open to anyone with the link
-file_id <-  "1EJc1LbdqN2dyjG1x-GZB8U2n1DshUGh2" 
-
+file_id <-  "1RztkWdZEq9pGXdhnMZp-B9hpOEQjqQ4E"
+  # SEV "1EJc1LbdqN2dyjG1x-GZB8U2n1DshUGh2" 
   # VCR Flora Manipulation "1ohOuZ_2YXsohG2cK7bDsvzALJIkOQJdV"
   # GCE Clear Cutting "1JFQQbocvR653UvkONsF_HPD6gt6E8xeK"
   # NTL Nutrient Addition "1aRxYd1DVEt2oq5zryWk8jqeW61DgPlnx"
@@ -46,11 +46,11 @@ summarize_by_columns <- function(data, group_cols) {
 }
 
 # use command unique(df$block) to find variables that needs to be summarized
-sum_df <- summarize_by_columns(df, c("year","season","plot","quad","treatment"))
+sum_df <- summarize_by_columns(df, c("year","block","snow","N","temp"))
 head(sum_df) 
 # saving the processed data to a new spreadsheet and put it into the ready folder!
 # Sip your coffee and carry on to the next dataset!
 
-write.csv(sum_df, "Ready_data/SEV_Aboveground_Biomass_2004-2023_gm2_processed.csv", row.names = FALSE)
+write.csv(sum_df, "Ready_data/NWT_AbovegroundBiomass_2006-2019_gm2_processed.csv", row.names = FALSE)
 
 
