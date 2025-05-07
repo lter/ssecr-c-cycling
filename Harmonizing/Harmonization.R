b40d8f5552dd141d66cf095a2e8e71a3a6b2e9ff
Ian-Yiyang <yiyangxu@bu.edu>
2025-05-07 12:02:19 -0400

    started data harmonization process created test_key created initial harmonization script

---
+++ Harmonizing/Harmonization.R
@@ -0,0 +1,16 @@
+# Use the command lines below to install or update "ltertools"
+# install.packages("devtools")
+# devtools::install_github("lter/ltertools")
+# more harmonization tips at: https://lter.github.io/ltertools/articles/ltertools.html
+
+
+
+# Generate a column key with "guesses" at tidy column names
+test_key <- ltertools::begin_key(raw_folder = "Ready_data", data_format = "csv", 
+                                 guess_tidy = TRUE)
+
+# Examine what that generated
+test_key
+
+# Write the newly generated test key into a csv file
+write.csv(sum_df, "Harmonizing/test_key.csv", row.names = FALSE)
