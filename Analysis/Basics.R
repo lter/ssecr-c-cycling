5f2f4084bb34a724ea5ba9f65e572581ffc2529b
Ian-Yiyang <yiyangxu@bu.edu>
2025-05-22 00:19:19 -0400

    Tring to get some treatment sorted

--- Analysis/Basics.R
+++ Analysis/Basics.R
@@ -1,15 +1,23 @@
+library(dplyr)
+library(readr)
 # Just visualizing the data
 df <- read.csv("Harmonizing/harmonized_Ian.csv")
 file.name <- unique(df$source)
-file_name <- file.name[7]
-single.data <-filter(df,df$source==file_name)
+file_name <- file.name[2]
+single.data <- filter(df,df$source==file_name)
 treatment <- unique(single.data$Treatment)
 Trmt.data <- filter(df,df$source==file_name
-                      & df$Treatment==treatment[3])
+                      & df$Treatment==treatment[5])
 Cntl.data <- filter(df,df$source==file_name
-                    & df$Treatment==treatment[2])
-Trmt.data <- summarize_by_columns(Trmt.data, c("Date"))
-Cntl.data <- summarize_by_columns(Cntl.data, c("Date"))
+                    & df$Treatment==treatment[1])
+res.data <- data.frame(Response = Trmt.data$Biomass/Cntl.data$Biomass,
+                 Date = Trmt.data$Date)
+# Trmt.data <- summarize_by_columns(Trmt.data, c("Date"))
+# Cntl.data <- summarize_by_columns(Cntl.data, c("Date"))
 # Cntl.data <- head(Cntl.data,-1)
-Response <- Trmt.data$Biomass_mean - Cntl.data$Biomass_mean
-plot(Response ~ Trmt.data$Date)
+#boxplot(Response ~ Trmt.data$Date)
+
+## ggplot
+ggplot(res.data,aes(Date, Response)) +
+       stat_summary(geom = "line", fun.y = mean) +
+       stat_summary(geom = "ribbon", fun.data = mean_cl_normal, alpha = 0.3)
