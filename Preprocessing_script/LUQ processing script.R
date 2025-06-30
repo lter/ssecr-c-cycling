9b1ad7cb6cee403298a09402f838d8e6ea545535
Carla López Lloreda <carla.lpez09@gmail.com>
2025-06-30 11:30:45 -0400

    finished LUQ GHG fluxes processing script

--- Preprocessing_script/LUQ processing script.R
+++ Preprocessing_script/LUQ processing script.R
@@ -1,8 +1,35 @@
+# Carla López Lloreda
+
+# load libraries
 library(googledrive)
 library(readr)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+# function to summarize
+summarize_by_columns <- function(data, group_cols) {
+  data %>%
+    group_by(across(all_of(group_cols))) %>%
+    summarise(across(where(is.numeric), list(
+      sd   = ~sd(.x, na.rm = TRUE),
+      mean = ~mean(.x, na.rm = TRUE)
+    ), .names = "{.col}_{.fn}"),
+    .groups = "drop")
+}
+
+summarize_by_columns <- function(data, group_cols) {
+  data %>%
+    group_by(across(all_of(group_cols))) %>%
+    summarise(across(where(is.numeric), list(
+      mean = ~mean(.x, na.rm = TRUE),
+      sd   = ~sd(.x, na.rm = TRUE)
+    ), .names = "{.col}_{.fn}"),
+    .groups = "drop")
+}
 
 # Authenticate with Google Drive (this will open a browser window)
-drive_auth()
+# drive_auth()
 
 # File ID from the Google Drive URL
 # The ID is the part after /folders/ in your URL
@@ -18,5 +45,21 @@ drive_download(file = file_info$id, path = temp_file, overwrite = TRUE)
 # Read the CSV file
 data <- read_csv(temp_file)
 
-summary(data)
-head(data)
+# check out columns and summary
+# summary(data)
+# head(data)
+
+# fix date
+data$DATE <- as.POSIXct(data$DATE, format = "%m/%d/%Y")
+
+# select columns and separate out info in location column
+data <- data %>%
+  select(-YEAR, -MONTH) %>%
+  separate("Block-plot-rep", into = c("Block", "Plot", "Rep"), sep = "-", remove = FALSE) %>%
+  mutate(Block_Plot = paste(Block, Plot, sep = "-"))
+
+# summarize means and sd of the dataset
+sum_df <- summarize_by_columns(data2, c("Block_Plot", "DATE"))
+
+# save file
+write.csv(sum_df, "Ready_data/LUQ_GHG_fluxes_2003-2010.csv")
