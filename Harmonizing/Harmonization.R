2a008a428de293eb5aa6ca1754fa67a01022a419
Julie <sg4122@columbia.edu>
2025-10-10 13:54:33 -0400

    harmonized datafiles as of october 10th 2025

--- Harmonizing/Harmonization.R
+++ Harmonizing/Harmonization.R
@@ -28,4 +28,4 @@ harmony <- ltertools::harmonize(key = Column_key, raw_folder = "Ready_data",
 utils::str(harmony)
 
 # Write the newly generated harmony data into a csv file
-write.csv(harmony, "Harmonizing/harmonized_aug7.csv", row.names = FALSE)
+write.csv(harmony, "Harmonizing/harmonized_Oct10.csv", row.names = FALSE)
